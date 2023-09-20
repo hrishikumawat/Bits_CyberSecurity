ab4c04578f21354d752616648af289556217f8f8
hrishikumawat <kumawathrishi@gmail.com>
2023-09-20 23:47:47 +0530

    Update task1-1B_2.py

--- Task-1/1-1/task1-1B_2.py
+++ Task-1/1-1/task1-1B_2.py
@@ -1,4 +1,4 @@
-#task1.1B_3 Capture packets comes from or to go to a particular subnet. You can pick any subnet, such as 128.230.0.0/16; you should not pick the subnet that your VM is attached to
+#task1.1B_2 Capture packets comes from or to go to a particular subnet. You can pick any subnet, such as 128.230.0.0/16; you should not pick the subnet that your VM is attached to
 
 #!/usr/bin/env python3
 from scapy.all import *
